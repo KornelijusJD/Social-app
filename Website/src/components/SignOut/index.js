cf1cfc05144d6e8ac1cecbd84f3e2ea777ec8458
KornelijusJD <korn.drungys@gmail.com>
2019-11-25 11:07:35 +0000

    Styled Navigation and log out

--- Website/src/components/SignOut/index.js
+++ Website/src/components/SignOut/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './sout.css';
 import { withFirebase } from '../Firebase';
 const SignOutButton = ({ firebase }) => (
-  <button type="button" onClick={firebase.doSignOut}>
-    Sign Out
+  <button type="button" className={"sout"} onClick={firebase.doSignOut}>
+    <b>Sign Out</b>
   </button>
 );
 export default withFirebase(SignOutButton);
