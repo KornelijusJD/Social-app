b07ae5b5fb2d7b4bf583fe1050309cabfc7eb035
KornelijusJD <korn.drungys@gmail.com>
2019-12-15 02:14:33 +0000

    basic text in landing added. more will need to be added to introduce a visitor to our website

--- Website/src/components/Landing/index.js
+++ Website/src/components/Landing/index.js
@@ -1,12 +1,71 @@
 import React from 'react';
 import './landing.css';
-//import blackback from "./background black.jpeg";
-
 const Landing = () => (
-  <div className={"back"}>
+  <div className={"bigchung"}>
     <h1>Landing</h1>
-    
+    <h2>Hello visitor and welcome to Self-Care Social App</h2>
+    <h3>In this website we aim to make people informed about their mental and 
+      physical well-being. We will aim to do this by having articles and events
+      regularily posted up on the website for you to know about.
+    </h3>
+    {/*<p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>
+    <p> |</p>*/}
+
   </div>
 );
+
 
 export default Landing;
