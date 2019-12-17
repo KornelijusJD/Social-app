ae0b98f70074bb1f5b2f75862d862751af922caf
Rjokubynas <robertas.jokubynas.2018@mumail.ie>
2019-12-17 12:14:29 +0000

    Changed link that was 404 in Links index

--- Website/src/components/Links/index.js
+++ Website/src/components/Links/index.js
@@ -9,7 +9,7 @@ const Links = () => (
         <div className={"grid-item"}><img src={'https://www.mentalhealthireland.ie/wp-content/uploads/2015/09/mental-health-gp.jpg'} alt="" className={"images"}></img></div>
         <div className={"grid-item"}><img src={'https://www.mentalhealthireland.ie/wp-content/uploads/2015/09/mental-health-counselling.jpg'} alt="" className={"images"}></img></div>  
         <div className={"grid-item"}><a href="http://www.mentalhealthireland.ie/need-help-now/">Helpline numbers</a></div>
-        <div className={"grid-item"}><a href="http://www.yourmentalhealth.ie/Supports-services/Types-of-services/Services-you-access-directly/G-P-services/">GP</a></div>
+        <div className={"grid-item"}><a href="https://www.mentalhealthireland.ie/a-to-z/gps/">GP</a></div>
         <div className={"grid-item"}><a href="http://www.psychotherapycouncil.ie/">Mental Health Counseling</a></div>  
         <div className={"grid-item"}>List of numbers of help lines that provide relating to particular issues or at times of crisis </div>
         <div className={"grid-item"}>GP may provide support directly or refer you on to community mental health services. You can find a list of support GPs following the link</div>
