728d45a84483f4f5579bdc5bb71513cac6b71acc
killiansmyth17 <killiansmyth17@gmail.com>
2019-12-17 21:57:35 +0000

    Removed Home header from home page

--- Website/src/components/Home/index.js
+++ Website/src/components/Home/index.js
@@ -3,8 +3,6 @@ import { withAuthorization } from '../Session';
 import Article from '../Article';
 const HomePage = () => (
   <div>
-    <h1>Home Page</h1>
-    <p>The Home Page is accessible by every signed in user.</p>
     <Article />
   </div>
 );
